b0bdf08641f54cf7be4ae577455e5d8458863fb1
Majedul Hoque <rumanbsl@gmail.com>
2018-03-25 15:41:41 +0300

    change

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/rumanbsl/Yet-another-Vue-Boilerplate.git front-end && git clone https://github.com/rumanbsl/Yet-another-express-boilerplate.git back-end && docker-compose build && docker-compose up
+git clone https://github.com/rumanbsl/Yet-another-Vue-Boilerplate.git front-end && git clone https://github.com/rumanbsl/Yet-another-express-boilerplate.git back-end && docker-compose build
